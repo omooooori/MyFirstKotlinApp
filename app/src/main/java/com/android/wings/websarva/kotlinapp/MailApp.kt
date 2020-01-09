682bcb4cbceb452dd2658bcc871cded1dd400612
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2020-01-09 23:55:35 +0900

    Create Mail App page (Just UI)

---
+++ app/src/main/java/com/android/wings/websarva/kotlinapp/MailApp.kt
@@ -0,0 +1,12 @@
+package com.android.wings.websarva.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MailApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mail_app)
+    }
+}
