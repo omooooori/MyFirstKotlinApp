ac8ffe8ab117a944fb9dcf054671553e8506be5d
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2020-01-16 00:07:18 +0900

    Create Calculator App. Not completed.

---
+++ app/src/main/java/com/android/wings/websarva/kotlinapp/CalculatorApp.kt
@@ -0,0 +1,12 @@
+package com.android.wings.websarva.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator_app)
+    }
+}
