cefeacd4333dac8ec5288bc0bd5ffd9ae9403fb1
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2020-01-18 14:39:48 +0900

    Create second text edit to save the first number. (calculator)

--- app/src/main/java/com/android/wings/websarva/kotlinapp/CalculatorApp.kt
+++ app/src/main/java/com/android/wings/websarva/kotlinapp/CalculatorApp.kt
@@ -7,6 +7,7 @@ import android.widget.EditText
 
 class CalculatorApp : AppCompatActivity() {
 
+    // This initialization is called every time when I push the button.
     var number : String = ""
     var numberOne : Float = 0.0F
     var numberTwo : Float = 0.0F
@@ -22,112 +23,121 @@ class CalculatorApp : AppCompatActivity() {
 
     fun operationFunction(view: View) {
 
-        var editText : EditText = findViewById(R.id.edit_text)
+        var editText1 : EditText = findViewById(R.id.edit_text1)
+        var editText2 : EditText = findViewById(R.id.edit_text2)
 
         when(view.id) {
             R.id.button_one -> {
                 number += "1"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_two -> {
                 number += "2"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_three -> {
                 number += "3"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_four -> {
                 number += "4"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_five -> {
                 number += "5"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_six -> {
                 number += "6"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_seven -> {
                 number += "7"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_eight -> {
                 number += "8"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_nine -> {
                 number += "9"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_zero -> {
                 number += "0"
-                editText.setText(number)
+                editText2.setText(number)
             }
             R.id.button_clear -> {
                 number = ""
-                editText.setText("")
+                editText1.setText("")
+                editText2.setText("")
             }
             R.id.button_plus -> {
                 if (number.isNullOrBlank()) {
-                    editText.setText("")
+                    editText2.setText("")
                 } else {
                     numberOne = number.toFloat()
-                    editText?.setText("")
+                    editText1?.setText(numberOne.toString())
+                    editText2?.setText("")
                     isAddition = true
                     number = ""
                 }
             }
             R.id.button_minus -> {
                 if (number.isNullOrBlank()) {
-                    editText.setText("")
+                    editText2.setText("")
                 } else {
                     numberOne = number.toFloat()
-                    editText?.setText("")
+                    editText1?.setText(numberOne.toString())
+                    editText2?.setText("")
                     isSubdraw = true
                     number = ""
                 }
             }
             R.id.button_time -> {
                 if (number.isNullOrBlank()) {
-                    editText.setText("")
+                    editText2.setText("")
                 } else {
                     numberOne = number.toFloat()
-                    editText?.setText("")
+                    editText1?.setText(numberOne.toString())
+                    editText2?.setText("")
                     isTimes = true
                     number = ""
                 }
             }
             R.id.button_divide -> {
                 if (number.isNullOrBlank()) {
-                    editText.setText("")
+                    editText2.setText("")
                 } else {
                     numberOne = number.toFloat()
-                    editText?.setText("")
+                    editText1?.setText(numberOne.toString())
+                    editText2?.setText("")
                     isDivide = true
                     number = ""
                 }
             }
             R.id.button_equal -> {
+                if (number.isNullOrBlank()) {
+                    editText2.setText("")
+                } else {
+                    numberTwo = number.toFloat()
 
-                if (isAddition) {
-                    numberTwo = number.toFloat()
-                    editText?.setText((numberOne + numberTwo).toString())
+                    if (isAddition) {
+                        editText1?.setText((numberOne + numberTwo).toString())
+                    }
+                    if (isSubdraw) {
+                        editText1?.setText((numberOne - numberTwo).toString())
+                    }
+                    if (isTimes) {
+                        editText1?.setText((numberOne * numberTwo).toString())
+                    }
+                    if (isDivide) {
+                        editText1?.setText((numberOne / numberTwo).toString())
+                    }
                 }
-                if (isSubdraw) {
-                    numberTwo = number.toFloat()
-                    editText?.setText((numberOne - numberTwo).toString())
-                }
-                if (isTimes) {
-                    numberTwo = number.toFloat()
-                    editText?.setText((numberOne * numberTwo).toString())
-                }
-                if (isDivide) {
-                    numberTwo = number.toFloat()
-                    editText?.setText((numberOne / numberTwo).toString())
-                }
-
+                
+                number = ""
+                editText2?.setText("")
             }
         }
     }
