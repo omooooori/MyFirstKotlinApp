e35e0805412a7ee31d4eeafbbd7dcd1ce0e82919
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2020-01-21 23:58:09 +0900

    Practice of ConstraintLayout

---
+++ app/src/main/java/com/android/wings/websarva/kotlinapp/ConstraintLayoutApp.kt
@@ -0,0 +1,12 @@
+package com.android.wings.websarva.kotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintLayoutApp : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout_app)
+    }
+}
